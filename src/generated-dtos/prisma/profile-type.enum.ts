a9b889323a5a4f890101e4b717d1aca3434f7f2e
Ashik Rahman <ashrhmn99@gmail.com>
2023-06-30 17:14:15 +0600

    remove gens

--- src/generated-dtos/prisma/profile-type.enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum profile_type {
-    LANDLORD = "LANDLORD",
-    TENANT = "TENANT"
-}
-
-
-registerEnumType(profile_type, { name: 'profile_type', description: undefined })
