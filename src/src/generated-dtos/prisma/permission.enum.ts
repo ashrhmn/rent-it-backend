ec6bb5bf9422958bdb0310eb12b261914f930103
Ashik Rahman <ashrhmn99@gmail.com>
2023-07-01 15:27:28 +0600

    fix export path

--- src/src/generated-dtos/prisma/permission.enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum permission {
-    CREATE_USER = "CREATE_USER",
-    MODIFY_USER = "MODIFY_USER"
-}
-
-
-registerEnumType(permission, { name: 'permission', description: undefined })
